f70900ea8fbb8bebfb16257d93f85f9d171a079b
Dhruvi8302 <voradhruvi8@gmail.com>
2025-08-16 09:46:18 +0530

    Delete assignment/python/lab assignment/assignment 4 positive negative.py

--- assignment/python/lab assignment/assignment 4 positive negative.py
+++
@@ -1,7 +0,0 @@
-number=int(input("enter your number"))
-if number>0:
-    print(f"{number} is positive")
-if number==0:
-    print(f"{number} is zero")
-if number<0:
-    print(f"{number} is nagative")
